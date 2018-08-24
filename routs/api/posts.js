0a5ddee510bd8ef98a8539c9126a86218f045ca8
Kevin M <kevin@powersoft.tv>
2018-08-23 18:39:10 -0700

    Added posts routs and validations

--- routs/api/posts.js
+++ routs/api/posts.js
@@ -1,9 +1,45 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
 
-// @route   GET request to api/posts/test
+// Post model
+const Post = require("../../models/Post");
+
+// Validation
+const validatePostInput = require("../../validation/post");
+
+// @route   GET  api/posts/test
 // @desc    Tests post rout
 // @access  Public
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route   POST  api/posts
+// @desc    Create post
+// @access  Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors
+      return res.status(400).json(errors);
+    }
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id
+    });
+
+    newPost
+      .save()
+      .then(post => res.json(post))
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
