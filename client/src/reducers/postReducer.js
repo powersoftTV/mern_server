045eef11f57a70215a2f1acc220c0c7fb96ec003
Kevin M <kevin@powersoft.tv>
2018-09-29 16:11:42 -0700

    Added posts action and reducer

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,12 @@
+const initialState = {
+  posts: [],
+  post: [],
+  loading: false
+};
+
+export default function(state = initialState) {
+  switch (applicationCache.type) {
+    default:
+      return state;
+  }
+}
