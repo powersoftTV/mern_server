71a34eff217fa72a87e31650401960949fe3e98e
Kevin M <kevin@powersoft.tv>
2018-09-30 17:17:58 -0700

    added delete like and removelike actions

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,12 +1,42 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import classnames from "classnames";
 import { Link } from "react-router-dom";
+import { deletePost } from "../../actions/postActions";
+import { addLike } from "../../actions/postActions";
+import { removeLike } from "../../actions/postActions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faThumbsUp } from "@fortawesome/free-solid-svg-icons";
 
 class PostItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isliked: this.findUserLike(this.props.post.likes)
+    };
+  }
   onDeleteClick(id) {
-    console.log(id);
+    this.props.deletePost(id);
+  }
+
+  onLikeClick(id) {
+    //this.props.addLike(id);
+    this.setState({ isliked: !this.state.isliked });
+  }
+
+  onUnlikeClick(id) {
+    //this.props.removeLike(id);
+    this.setState({ isliked: !this.state.isliked });
+  }
+
+  findUserLike(likes) {
+    const { auth } = this.props;
+
+    if (likes.filter(like => like.user === auth.user.id).length > 0) {
+      return true;
+    } else {
+      return false;
+    }
   }
 
   render() {
@@ -28,11 +58,24 @@ class PostItem extends Component {
           </div>
           <div className="col-md-10">
             <p className="lead">{post.text}</p>
-            <button type="button" className="btn btn-light mr-1">
-              <i className="text-info fas fa-thumbs-up" />
+            <button
+              onClick={this.onLikeClick.bind(this, post._id)}
+              type="button"
+              className="btn btn-light mr-1"
+            >
+              {this.state.isliked ? (
+                <FontAwesomeIcon icon={faThumbsUp} className="text-info" />
+              ) : (
+                <FontAwesomeIcon icon={faThumbsUp} />
+              )}
+
               <span className="badge badge-light">{post.likes.length}</span>
             </button>
-            <button type="button" className="btn btn-light mr-1">
+            <button
+              onClick={this.onUnlikeClick.bind(this, post._id)}
+              type="button"
+              className="btn btn-light mr-1"
+            >
               <i className="text-secondary fas fa-thumbs-down" />
             </button>
             <Link to={`/post/${post._id}`} className="btn btn-info mr-1">
@@ -55,6 +98,9 @@ class PostItem extends Component {
 }
 
 PostItem.propTypes = {
+  deletePost: PropTypes.func.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   post: PropTypes.object.isRequired
 };
@@ -63,4 +109,7 @@ const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps)(PostItem);
+export default connect(
+  mapStateToProps,
+  { deletePost, addLike, removeLike }
+)(PostItem);
